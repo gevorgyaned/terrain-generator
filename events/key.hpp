657c8e1eb1b1581c89d6eefe20f1bfa8af26fd8f
gevorgyaned <eduardgev00@gmal.com>
2024-09-08 17:18:15 +0400

    user input class and key enum added

---
+++ events/key.hpp
@@ -0,0 +1,68 @@
+#ifndef KEY_HPP
+#define KEY_HPP
+
+#include <GLFW/glfw3.h>
+
+enum class Key {
+    A = GLFW_KEY_A,
+    B = GLFW_KEY_B,
+    C = GLFW_KEY_C,
+    D = GLFW_KEY_D,
+    E = GLFW_KEY_E,
+    F = GLFW_KEY_F,
+    G = GLFW_KEY_G,
+    H = GLFW_KEY_H,
+    I = GLFW_KEY_I,
+    J = GLFW_KEY_J,
+    K = GLFW_KEY_K,
+    L = GLFW_KEY_L,
+    M = GLFW_KEY_M,
+    N = GLFW_KEY_N,
+    O = GLFW_KEY_O,
+    P = GLFW_KEY_P,
+    Q = GLFW_KEY_Q,
+    R = GLFW_KEY_R,
+    S = GLFW_KEY_S,
+    T = GLFW_KEY_T,
+    U = GLFW_KEY_U,
+    V = GLFW_KEY_V,
+    W = GLFW_KEY_W,
+    X = GLFW_KEY_X,
+    Y = GLFW_KEY_Y,
+    Z = GLFW_KEY_Z,
+
+    Left  = GLFW_KEY_LEFT,
+    Right = GLFW_KEY_RIGHT,
+    Up    = GLFW_KEY_UP,
+    Down  = GLFW_KEY_DOWN,
+
+    Key0 = GLFW_KEY_0,
+    Key1 = GLFW_KEY_1,
+    Key2 = GLFW_KEY_2,
+    Key3 = GLFW_KEY_3,
+    Key4 = GLFW_KEY_4,
+    Key5 = GLFW_KEY_5,
+    Key6 = GLFW_KEY_6,
+    Key7 = GLFW_KEY_7,
+    Key8 = GLFW_KEY_8,
+    Key9 = GLFW_KEY_9,
+
+    Escape    = GLFW_KEY_ESCAPE,
+    Enter     = GLFW_KEY_ENTER,
+    Tab       = GLFW_KEY_TAB,
+    Backspace = GLFW_KEY_BACKSPACE,
+    Insert    = GLFW_KEY_INSERT,
+    Delete    = GLFW_KEY_DELETE,
+
+    Space      = GLFW_KEY_SPACE,
+    Apostrophe = GLFW_KEY_APOSTROPHE,
+    Comma      = GLFW_KEY_COMMA,
+    Minus      = GLFW_KEY_MINUS,
+    Period     = GLFW_KEY_PERIOD,
+    Slash      = GLFW_KEY_SLASH,
+    Semicolon  = GLFW_KEY_SEMICOLON,
+    Equal      = GLFW_KEY_EQUAL,
+};
+
+#endif /* KEY_HPP */
+
